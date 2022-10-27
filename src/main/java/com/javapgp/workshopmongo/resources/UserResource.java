0b27ac368a67122f97d8a4bc88062ba01d3b5073
Pedro Gomes <pedrogomespeixoto14@gmail.com>
2022-10-27 15:39:11 -0300

    UserDTO (Data Transfer Object)

---
+++ src/main/java/com/javapgp/workshopmongo/resources/UserResource.java
@@ -0,0 +1,31 @@
+package com.javapgp.workshopmongo.resources;
+
+import com.javapgp.workshopmongo.domain.User;
+import com.javapgp.workshopmongo.dto.UserDTO;
+import com.javapgp.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<UserDTO>> findAll(){
+        List<User> list = userService.findAll();
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
+    }
+
+}
