c9d1c82aad6be2c78446c60fd31b6ec3fa5537d6
Pedro Gomes <pedrogomespeixoto14@gmail.com>
2022-10-28 01:56:38 -0300

    Final Commit / finished project

--- src/main/java/com/javapgp/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/javapgp/workshopmongo/repository/PostRepository.java
@@ -2,8 +2,10 @@ package com.javapgp.workshopmongo.repository;
 
 import com.javapgp.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -11,4 +13,9 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/javapgp/workshopmongo/services/PostService.java
+++ src/main/java/com/javapgp/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.javapgp.workshopmongo.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -23,7 +24,12 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String text){
-        return PostRepository.findByTitleContainingIgnoreCase(text);
+        return PostRepository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return PostRepository.fullSearch(text, minDate, maxDate);
     }
 
 }
